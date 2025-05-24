466934344b8462331bd736cef7a2d4a2896ac121
MF <michael.francavilla@gmail.com>
2025-05-24 10:53:40 -0600

    update database tables

--- DB/proxyshopdb.sql
+++ DB/proxyshopdb.sql
@@ -16,14 +16,63 @@ CREATE SCHEMA IF NOT EXISTS `proxyshopdb` DEFAULT CHARACTER SET utf8 ;
 USE `proxyshopdb` ;
 
 -- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(100) NULL,
+  `enabled` TINYINT NULL,
+  `role` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `store`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `store` ;
+
+CREATE TABLE IF NOT EXISTS `store` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `logo_image_url` VARCHAR(2000) NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `item_to_buy`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `item_to_buy` ;
 
 CREATE TABLE IF NOT EXISTS `item_to_buy` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `sku_name` VARCHAR(2000) NULL,
-  PRIMARY KEY (`id`))
+  `sku` VARCHAR(200) NULL,
+  `user_id` INT NOT NULL,
+  `store_id` INT NOT NULL,
+  `name` VARCHAR(2000) NULL,
+  `description` TEXT NULL,
+  `image_url` VARCHAR(2000) NULL,
+  `last_purchased` DATETIME NULL,
+  `needed` TINYINT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_item_to_buy_user_idx` (`user_id` ASC) VISIBLE,
+  INDEX `fk_item_to_buy_store1_idx` (`store_id` ASC) VISIBLE,
+  CONSTRAINT `fk_item_to_buy_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_item_to_buy_store1`
+    FOREIGN KEY (`store_id`)
+    REFERENCES `store` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 SET SQL_MODE = '';
@@ -38,11 +87,34 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
+-- Data for table `user`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `proxyshopdb`;
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (1, 'mike', 'mike', 1, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `enabled`, `role`) VALUES (2, 'rob', 'rob', 1, NULL);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `store`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `proxyshopdb`;
+INSERT INTO `store` (`id`, `name`, `logo_image_url`, `description`) VALUES (1, 'Trader Joe\'s', NULL, 'Good stuff');
+INSERT INTO `store` (`id`, `name`, `logo_image_url`, `description`) VALUES (2, 'Costco', NULL, 'Big stuff');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `item_to_buy`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `proxyshopdb`;
-INSERT INTO `item_to_buy` (`id`, `sku_name`) VALUES (1, 'TVP');
+INSERT INTO `item_to_buy` (`id`, `sku`, `user_id`, `store_id`, `name`, `description`, `image_url`, `last_purchased`, `needed`) VALUES (1, '12', 1, 1, 'Textured Vegetable Protien', NULL, NULL, NULL, NULL);
+INSERT INTO `item_to_buy` (`id`, `sku`, `user_id`, `store_id`, `name`, `description`, `image_url`, `last_purchased`, `needed`) VALUES (2, '12345', 2, 2, '55 gallon drum of mayo', NULL, NULL, NULL, NULL);
 
 COMMIT;
 
